491b3e5314a43b80fd53a0b5bce1e5186d6edb00
angel-15 <angelrestifo36@gmail.com>
2021-07-17 14:04:07 +0000

    Generador de dominios de paginas Web

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#Domain").innerHTML = domainGenerator();
+  });
+  console.log("Angel Restifo - Generador de dominios de paginas Web");
+};
+
+let domainGenerator = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = [".com", ".net", ".us", ".io"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let adjIndex = Math.floor(Math.random() * adj.length);
+  let nounIndex = Math.floor(Math.random() * noun.length);
+  let extIndex = Math.floor(Math.random() * ext.length);
+
+  return (
+    pronoun[pronounIndex] + adj[adjIndex] + noun[nounIndex] + ext[extIndex]
+  );
 };
